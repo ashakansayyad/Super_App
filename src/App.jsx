954a781e4aabe3b10494aaa44329a0fcd7a05333
Ashakan Sayyad <sayyadashakan1002@gmail.com>
2024-12-05 13:30:32 +0530

    added vercel file

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,63 @@
-import React from 'react'
-import RegistrationPage from './pages/RegistrationPage'
-import NotFoundPage from './pages/NotFoundPage'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import GenrePage from './pages/GenrePage'
-import Carousel from './pages/Carousel'
-import DashbordPage from './pages/DashbordPage'
-import MoviePage from './pages/MoviePage'
-import { Navigate } from 'react-router-dom'
-import { AppContext } from './context/AppContext'
-import { useContext } from 'react'
-import './App.css'
+import React from "react";
+import RegistrationPage from "./pages/RegistrationPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GenrePage from "./pages/GenrePage";
+import Carousel from "./pages/Carousel";
+import DashbordPage from "./pages/DashbordPage";
+import MoviePage from "./pages/MoviePage";
+import { Navigate } from "react-router-dom";
+import { AppContext } from "./context/AppContext";
+import { useContext } from "react";
+import "./App.css";
 
-const ProtectedRoute =({children})=>{
-  const {user}=useContext(AppContext);
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(AppContext);
 
-  return user ? children : <Navigate to="/RegistrationPage"/>
+  return user ? children : <Navigate to="/RegistrationPage" />;
 };
 
 export default function App() {
-
-
-
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-      <Route  path='/' element={<RegistrationPage/>} />
-      <Route path='/Genre' element={
-        <ProtectedRoute>
-          <GenrePage/>
-        </ProtectedRoute>
-
-      } />
-      <Route path='/carousel' element={
-        <ProtectedRoute>
-          <Carousel/>
-        </ProtectedRoute>
-      } />
-      <Route path='/dashbord' element={
-          <ProtectedRoute>
-          <DashbordPage/>
-        </ProtectedRoute>
-       }/>
-      <Route path='/movies' element={
-         <ProtectedRoute>
-        <MoviePage/>
-       </ProtectedRoute>
-        }/>
-      <Route  path='*' element={<NotFoundPage/>} />
-
-    </Routes>
-    
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RegistrationPage />} />
+          <Route
+            path="/Genre"
+            element={
+              <ProtectedRoute>
+                <GenrePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/carousel"
+            element={
+              <ProtectedRoute>
+                <Carousel />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashbord"
+            element={
+              <ProtectedRoute>
+                <DashbordPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/movies"
+            element={
+              <ProtectedRoute>
+                <MoviePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
--- src/utils/formatDateAndTime.js
+++ src/utils/formatDateAndTime.js
@@ -1,19 +1,17 @@
-const formatDateAndTime=()=>{
+const formatDateAndTime = () => {
+  const currentDate = new Date();
+  const formatDate = currentDate.toLocaleDateString();
 
-    const currentDate = new Date();
-    const formatDate = currentDate.toLocaleDateString();
-    
-    
-    
-    const currentTime = new Date();
-    const formatTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
-    
-    
+  const currentTime = new Date();
+  const formatTime = currentTime.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
-    return {
-       date: formatDate,
-        time:formatTime,
-    }
-}
+  return {
+    date: formatDate,
+    time: formatTime,
+  };
+};
 
 export default formatDateAndTime;
